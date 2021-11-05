87539b4baf49d7b5f950f68aca219c9c51f85899
lukaob <59558130+lukaob@users.noreply.github.com>
2021-11-05 18:01:36 +0100

    Update MountDataDisk.ps1

--- MountDataDisk.ps1
+++ MountDataDisk.ps1
@@ -1,3 +1,2 @@
-Get-Disk | Where partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle GPT -PassThru | '
-New-Partition -AssignDriveLetter -UseMaximumSize | '
-Format-Volume -FileSystem NTFS -NewFileSystemLabel "datadisk" -Confirm:$false
+Get-Disk | Where OperationalStatus -eq 'Offline' | Set-Disk -IsOffline $false
+Get-Disk | Where partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle GPT -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "datadisk" -Confirm:$false
